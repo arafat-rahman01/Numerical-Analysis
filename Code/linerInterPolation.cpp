bf0a30d1b1d216d5e81723665c2984ab1ba0ee88
MD. Arafat Rahman <aabrarbin851@gmail.com>
2025-07-18 12:17:53 +0600

    Update linerInterPolation.cpp

--- Code/linerInterPolation.cpp
+++ Code/linerInterPolation.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
     double x,x0,x1,fx0,fx1;
     cin>>x>>x0>>x1>>fx0>>fx1;
-    double fx=fx0+((fx1-fx0)/x1-x0)*(x-x0);
+    double fx=fx0+((fx1-fx0)/(x1-x0))*(x-x0);
     cout<<fx<<endl;
 }
